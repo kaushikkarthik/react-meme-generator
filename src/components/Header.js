4e98d8a54c674af010413500c4f9fe2cb99a6019
Kaushik <47927407+kaushikkarthik@users.noreply.github.com>
2022-07-06 20:23:59 +0800

    text state change

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
                 src="./images/troll-face.png" 
                 className="header--image"
                 alt=""
-            />
+            />      
             <h2 className="header--title">Meme Generator</h2>
             <h3 className="header--project">
                 <a style={{"font-size":"1rem", "color":"white"}}
